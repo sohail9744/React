102814aed9bc046c7233983d8a964ada45e81cd8
Mohammad Sohail Khan <mohammadsohailkhan999@gmail.com>
2023-10-03 10:14:01 +0530

    Intial Startup

---
+++ inzpireuProject/src/LoginPageLayout/LoginPage.jsx
@@ -0,0 +1,15 @@
+export default function Login() {
+  return (
+    <>
+      <main className="flex h-screen justify-center items-center">
+        <p className="w-3/4 text-2xl font-bold underline text-blue-700">
+          Embark on this exciting journey with an open heart and a curious
+          mind, for in the world of this project, every challenge is an
+          opportunity, every question is a chance to learn, and every effort
+          contributes to our shared success. Together, let turn possibilities
+          into realities and make our project story one worth
+        </p>
+      </main>
+    </>
+  );
+}
